8ef1df6588a451600d1aa08b2e29f15b0a165c90
Vannida Lim <vannida.lim@gmail.com>
2019-10-15 17:15:56 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,52 @@
 class Artist
 
   attr_reader :name, :years_experience
+  @@all = []
 
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+  end
+
+  def self.all 
+    @@all 
+  end 
+
+  def paintings
+    Painting.all.select do |painting|
+      #binding.pry 
+      painting.artist == self
+    end
+  end
+
+  def galleries
+    paintings.map do |painting|
+      painting.gallery
+    end
+  end
+
+  def cities
+    galleries.map do |gallery|
+      gallery.city
+    end
+  end
+
+  def self.total_experience
+    paintings.max_by do |painting|
+      binding.pry
+    end
+  end
+
+  def self.most_prolific
+    self.all.max_by do |artist|
+      artist.paintings.count/artist.years_experience
+    end
+  end
+
+  def self.create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+    binding.pry
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,16 @@
 require_relative '../config/environment.rb'
 
+van_gogh = Artist.new("Van Gogh", 10)
+monet = Artist.new("Monet", 30)
+picasso = Artist.new("Picasso", 12)
 
+tate_modern = Gallery.new("Tate Modern", "London")
+flatiron = Gallery.new("Flatiron", "Dumbo")
+moma = Gallery.new("MoMa","Manhattan")
 
+starry_night = Painting.new("Starry Night", 34, van_gogh, tate_modern)
+lilies = Painting.new("Lilies", 100, monet, tate_modern)
+cube = Painting.new("Cube", 600, picasso, moma)
 
 binding.pry
 
