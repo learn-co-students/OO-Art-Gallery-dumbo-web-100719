5ac4ebcf6a9350bac4ddda956cbff738cddba015
Diego Martinez <Diego@Diegos-MacBook-Pro.local>
2019-10-15 22:41:18 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,42 @@
 class Artist
 
   attr_reader :name, :years_experience
-
+  @@all = []
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
+
+  def create_painting(title,price,gallery)
+    Painting.new(title,price,self,gallery)
+  end
+  
+  def paintings
+    Painting.all.select {|painting| painting.artist == self}
+  end
+
+  def galleries
+    paintings.map {|painting| painting.gallery}
+  end
+
+  def cities
+    galleries.map {|gallery| gallery.city}
+  end
+
+  def self.total_experience
+    t_exp = 0
+    self.all.map {|artist| t_exp += artist.years_experience}.last
+  end
+
+  def self.most_prolific
+    
+  
+  end
+
+  def self.all
+    @@all
+  end
+
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,30 @@
 require_relative '../config/environment.rb'
 
+picasso = Artist.new("Picasso", 40)
+pollock = Artist.new("Pollock", 34)
+monet = Artist.new("Monet", 55)
 
+moma = Gallery.new("MoMa", "NYC")
+louvre = Gallery.new("Louvre", "Paris")
+met = Gallery.new("Met", "NYC")
+
+
+monet.create_painting("poppies", 5000, louvre)
+picasso.create_painting("guernica", 4000, moma)
+pollock.create_painting("mural", 2000, moma)
+pollock.create_painting("convergence", 1000, moma)
+monet.create_painting("lady", 3500, met)
+
+monet_paintings = monet.paintings
+monet_galleries = monet.galleries
+monet_gal_cities = monet.cities
+
+artists_tot_exp = Artist.total_experience
+
+moma_paintings = moma.paintings
+moma_artists = moma.artists 
+moma_artist_names = moma.artist_names
+moma_most_exp = moma.most_expensive_painting
 
 
 binding.pry
